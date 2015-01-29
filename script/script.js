e2d4158e2ef249c74d1a767e1ae287baa87855b1
laurenpawell <lauren.pawell@gmail.com>
2015-01-29 15:52:29 -0800

    More progress on win logic, but condition is true no matter who plays

--- script/script.js
+++ script/script.js
@@ -2,6 +2,12 @@ var app = angular.module("ticTacToeApp", []);
 
 app.controller("ticTacToeCtrl", function($scope){
 
+	//var ref = new Firebase("https://angular-tic-tac-toe.firebaseio.com/");
+    // create an AngularFire reference to the data
+    //var sync = $firebase(ref);
+    // download the data into a local object
+    // DON'T FORGET TO DO!  $scope.names = sync.$asArray();
+
 	// This array describes the 9 possible places on the board.
 	$scope.board = ["", "", "", "", "", "", "", "", ""];
 
@@ -27,7 +33,21 @@ app.controller("ticTacToeCtrl", function($scope){
     	$scope.oWins = false;
 
     	// If true, it's a cat's game.
-    	$scope.tie = false;
+    	//$scope.tie = false;
+
+     // This function starts to find the winning status (or lack thereof).
+
+    $scope.checkGameStatus = function(play){
+		if(play == "X"){
+			$scope.xWins = true;
+			alert("X Wins!");
+		}
+		else if(play == "O"){
+			$scope.oWins = true;
+			alert("O Wins!");
+		}
+
+	};
 
 
     // This sets the possible win conditions.
@@ -35,42 +55,37 @@ app.controller("ticTacToeCtrl", function($scope){
     $scope.winConditions = function(play) {
     	
     	if($scope.board[0] && $scope.board[1] && $scope.board[2]) {
-    		alert("it's a win!");
+    		$scope.checkGameStatus(play);
     	}
     	else if($scope.board[3] && $scope.board[4] && $scope.board[5] ) {
-    		alert("it's a win!");
+    		$scope.checkGameStatus(play);
     	}
     	else if($scope.board[6] && $scope.board[7] && $scope.board[8] ) {
-    		alert("it's a win!");
+    		$scope.checkGameStatus(play);
     	}
     	else if($scope.board[0] && $scope.board[3] && $scope.board[6] ) {
-    		alert("it's a win!");
+    		$scope.checkGameStatus(play);
     	}
 
     	else if($scope.board[1] && $scope.board[4] && $scope.board[7] ) {
-    		alert("it's a win!");
+    		$scope.checkGameStatus(play);
     	}
     	else if($scope.board[2] && $scope.board[5] && $scope.board[8] ) {
-    		alert("it's a win!");
+    		$scope.checkGameStatus(play);
     	}
     	else if($scope.board[0] && $scope.board[4] && $scope.board[8] ) {
-    		alert("it's a win!");
+    		$scope.checkGameStatus(play);
     	}
     	else if($scope.board[2] && $scope.board[4] && $scope.board[6] ) {
-    		alert("it's a win!");
+    		$scope.checkGameStatus(play);
     	}
     	else  {
-    		// it's a tie
+    		//alert("It's a tie!")
     	}
 
     };
 
   	// [0,1,2] [3,4,5] [6,7,8] [0,3,6] [1,4,7] [2,5,8] [0,4,8] [2,4,6]
-
-    
-    // This function starts to find the winning status (or lack thereof).
-
-    
 
 
 });
