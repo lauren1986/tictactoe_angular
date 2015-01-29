0643b1740f54e0e2e60697516587e98fad52046b
laurenpawell <lauren.pawell@gmail.com>
2015-01-28 20:45:26 -0800

    Started win logic

--- script/script.js
+++ script/script.js
@@ -8,19 +8,15 @@ app.controller("ticTacToeCtrl", function($scope){
 	// This defines that the turn number starts at 0.
 	$scope.turnNumber = 0;
 
-	// This function allows for alternating turns and inserting X & 0.
+	// This function allows for alternating turns, a max of 9, and inserting X & 0.
     $scope.makeMove = function(idx){
     	if ($scope.turnNumber < 9) {
-	        $scope.board[idx] = (($scope.turnNumber % 2) == 0 ? "X" : "O");
+	        var play = (($scope.turnNumber % 2) == 0 ? "X" : "O");
+	        $scope.board[idx] = play;
+	        $scope.winConditions(play);
 	        $scope.turnNumber++;
 	    }
     };
-
-    // This limits the number of turns to 9.
-
-    // This sets the possible win conditions.
-
-
 
     // This describes the possible game end scenarios.
 
@@ -33,13 +29,48 @@ app.controller("ticTacToeCtrl", function($scope){
     	// If true, it's a cat's game.
     	$scope.tie = false;
 
-    // This function starts to find the winning status (or lack thereof).
 
-    $scope.winningStatus = function() {
+    // This sets the possible win conditions.
+
+    $scope.winConditions = function(play) {
+    	
+    	if($scope.board[0] && $scope.board[1] && $scope.board[2]) {
+    		alert("it's a win!");
+    	}
+    	else if($scope.board[3] && $scope.board[4] && $scope.board[5] ) {
+    		alert("it's a win!");
+    	}
+    	else if($scope.board[6] && $scope.board[7] && $scope.board[8] ) {
+    		alert("it's a win!");
+    	}
+    	else if($scope.board[0] && $scope.board[3] && $scope.board[6] ) {
+    		alert("it's a win!");
+    	}
+
+    	else if($scope.board[1] && $scope.board[4] && $scope.board[7] ) {
+    		alert("it's a win!");
+    	}
+    	else if($scope.board[2] && $scope.board[5] && $scope.board[8] ) {
+    		alert("it's a win!");
+    	}
+    	else if($scope.board[0] && $scope.board[4] && $scope.board[8] ) {
+    		alert("it's a win!");
+    	}
+    	else if($scope.board[2] && $scope.board[4] && $scope.board[6] ) {
+    		alert("it's a win!");
+    	}
+    	else  {
+    		// it's a tie
+    	}
 
     };
+
+  	// [0,1,2] [3,4,5] [6,7,8] [0,3,6] [1,4,7] [2,5,8] [0,4,8] [2,4,6]
+
     
-    // This 
+    // This function starts to find the winning status (or lack thereof).
+
+    
 
 
 });
