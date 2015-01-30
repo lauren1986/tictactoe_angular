db2040b1b995c0b835422bf4839a9372ca6a6c86
laurenpawell <lauren.pawell@gmail.com>
2015-01-30 15:36:16 -0800

    X win logic fixed

--- script/script.js
+++ script/script.js
@@ -1,12 +1,14 @@
-var app = angular.module("ticTacToeApp", []);
+var app = angular.module("ticTacToeApp", ["firebase"]);
 
-app.controller("ticTacToeCtrl", function($scope){
+app.controller("ticTacToeCtrl", function($scope, $firebase){
 
-	//var ref = new Firebase("https://angular-tic-tac-toe.firebaseio.com/");
+
+	// Set up the board in firebase.
+	var ref = new Firebase("https://angular-tic-tac-toe.firebaseapp.com/board");
     // create an AngularFire reference to the data
-    //var sync = $firebase(ref);
+    var sync = $firebase(ref);
     // download the data into a local object
-    // DON'T FORGET TO DO!  $scope.names = sync.$asArray();
+    $scope.board = sync.$asArray();
 
 	// This array describes the 9 possible places on the board.
 	$scope.board = ["", "", "", "", "", "", "", "", ""];
@@ -38,9 +40,13 @@ app.controller("ticTacToeCtrl", function($scope){
     	if ($scope.turnNumber < 9) {
     		if (($scope.board[idx] !='X') && ($scope.board[idx] !='O')){
 
-		        var play = (($scope.turnNumber % 2) == 0 ? "X" : "O");
-		        $scope.board[idx] = play;
-		        $scope.winConditions(play);
+						if (($scope.turnNumber % 2) == 0) {
+							$scope.board[idx] = "X";
+						}
+						else if (($scope.turnNumber % 2) != 0) {
+							$scope.board[idx] = "O";
+						}
+		        $scope.winConditions();
 		        $scope.turnNumber++;
 		    }
 		}
@@ -59,53 +65,90 @@ app.controller("ticTacToeCtrl", function($scope){
 
      // This function starts to find the winning status (or lack thereof).
 
-    $scope.checkGameStatus = function(play){
-		if(play == "X"){
-			$scope.xWins = true;
-			alert("X Wins!");
-		}
-		else if(play == "O"){
-			$scope.oWins = true;
-			alert("O Wins!");
-		}
-		//else {
-			//$scope.makeMove != null;
-			//alert("it's a tie!")
-		//}
-	};
+  //   $scope.checkGameStatus = function(play){
+	// 	if(play == "X"){
+	// 		$scope.xWins = true;
+	// 		alert("X Wins!");
+	// 	}
+	// 	else if(play == "O"){
+	// 		$scope.oWins = true;
+	// 		alert("O Wins!");
+	// 	}
+	// 	//else {
+	// 		//$scope.makeMove != null;
+	// 		//alert("it's a tie!")
+	// 	//}
+	// };
 
 
     // This sets the possible win conditions.
 
-    $scope.winConditions = function(play) {
-	    	if($scope.board[0] && $scope.board[1] && $scope.board[2] ) {
-	    		$scope.checkGameStatus(play);
-	    	}
-	    	else if($scope.board[3] && $scope.board[4] && $scope.board[5] ) {
-	    		$scope.checkGameStatus(play);
-	    	}
-	    	else if($scope.board[6] && $scope.board[7] && $scope.board[8] ) {
-	    		$scope.checkGameStatus(play);
-	    	}
-	    	else if($scope.board[0] && $scope.board[3] && $scope.board[6] ) {
-	    		$scope.checkGameStatus(play);
-	    	}
-
-	    	else if($scope.board[1] && $scope.board[4] && $scope.board[7] ) {
-	    		$scope.checkGameStatus(play);
-	    	}
-	    	else if($scope.board[2] && $scope.board[5] && $scope.board[8] ) {
-	    		$scope.checkGameStatus(play);
-	    	}
-	    	else if($scope.board[0] && $scope.board[4] && $scope.board[8] ) {
-	    		$scope.checkGameStatus(play);
-	    	}
-	    	else if($scope.board[2] && $scope.board[4] && $scope.board[6] ) {
-	    		$scope.checkGameStatus(play);
-	    	}
-	    	else  {
-	    		//alert("It's a tie!")
-	    	}
+    $scope.winConditions = function() {
+			if (($scope.turnNumber % 2) == 0) {
+	    	if(($scope.board[0] == "X") && ($scope.board[1] == "X") && ($scope.board[2]=="X") ) {
+					$scope.xWins = true;
+					console.log("X Wins!");
+	    		}
+	    	else if(($scope.board[3] == "X") && ($scope.board[4] == "X") && ($scope.board[5]=="X") ) {
+	    		$scope.xWins = true;
+					console.log("X Wins!");
+					}
+	    	else if(($scope.board[6] == "X") && ($scope.board[7]=="X") && ($scope.board[8]=="X") ) {
+	    	  $scope.xWins = true;
+				  console.log("X Wins!");
+	    		}
+	    	else if(($scope.board[0] == "X") && ($scope.board[3]=="X") && ($scope.board[6]=="X") ) {
+					$scope.xWins = true;
+					console.log("X Wins!");
+	    	  }
+	    	else if(($scope.board[1] =="X") && ($scope.board[4]=="X") && ($scope.board[7]=="X") ) {
+	    	   $scope.xWins = true;
+					 console.log("X Wins!");
+	    		}
+	    	else if(($scope.board[2]=="X") && ($scope.board[5]=="X") && ($scope.board[8]=="X") ) {
+	    		$scope.xWins = true;
+					console.log("X Wins!");
+	    		}
+	    	else if(($scope.board[0]=="X") && ($scope.board[4]=="X") && ($scope.board[8]=="X") ) {
+					$scope.xWins = true;
+					console.log("X Wins!");
+	    	 	}
+	    	else if(($scope.board[2]=="X") && ($scope.board[4]=="X") && ($scope.board[6]=="X") ) {
+					$scope.xWins = true;
+					console.log("X Wins!");
+					}
+			}
+			else if (($scope.turnNumber % 2) != 0) {
+				if(($scope.board[0] == "O") && ($scope.board[1] == "O") && ($scope.board[2]=="O") ) {
+					$scope.oWins = true;
+					console.log("O Wins!")
+				}
+				// else if($scope.board[3] && $scope.board[4] && $scope.board[5] ) {
+				// 	$scope.checkGameStatus();
+				// }
+				// else if($scope.board[6] && $scope.board[7] && $scope.board[8] ) {
+				// 	$scope.checkGameStatus();
+				// }
+				// else if($scope.board[0] && $scope.board[3] && $scope.board[6] ) {
+				// 	$scope.checkGameStatus();
+				// }
+				//
+				// else if($scope.board[1] && $scope.board[4] && $scope.board[7] ) {
+				// 	$scope.checkGameStatus(play);
+				// }
+				// else if($scope.board[2] && $scope.board[5] && $scope.board[8] ) {
+				// 	$scope.checkGameStatus(play);
+				// }
+				// else if($scope.board[0] && $scope.board[4] && $scope.board[8] ) {
+				// 	$scope.checkGameStatus(play);
+				// }
+				// else if($scope.board[2] && $scope.board[4] && $scope.board[6] ) {
+				// 	$scope.checkGameStatus(play);
+				// }
+				// else  {
+				// 	//alert("It's a tie!")
+				// }
+			}
     };
 
   	// [0,1,2] [3,4,5] [6,7,8] [0,3,6] [1,4,7] [2,5,8] [0,4,8] [2,4,6]
