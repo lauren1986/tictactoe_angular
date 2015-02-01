73011c1ae4e0c239a6da6ee85a0a6b20d16c9a9f
laurenpawell <lauren.pawell@gmail.com>
2015-02-01 14:01:45 -0800

    Added entertaining content to distract from bugs

--- script/script.js
+++ script/script.js
@@ -48,16 +48,14 @@ ticTacToeApp.controller("ticTacToeCtrl", function($scope, $firebase) {
 	// Creates and saves the turns to Firebase.
 
 	$scope.turns.$loaded(function(){
-		if ($scope.counter == 0){
+		if ($scope.turns.length==0){
 			$scope.turns.$add({numMoves: 0});
 		}
 		else {
-			$scope.counter[0].numMoves=0;
+			$scope.turns[0].numMoves=0;
 			$scope.turns.$save(0);
 		}
 	});
-
-	// Creates and saves __________ to Firebase.
 
 
 // This defines that the turn number starts at 0.
@@ -81,20 +79,19 @@ ticTacToeApp.controller("ticTacToeCtrl", function($scope, $firebase) {
 
 // This function allows for alternating turns, a max of 9, no duplicating turns, and inserting X & 0.
     $scope.makeMove = function(idx){
-    	if ($scope.turns[0].numMoves == 0) {
-    		if (($scope.board[idx].moveByPlayer !='X') && ($scope.board[idx].moveByPlayer !='O') ){
+    	if ($scope.turnNumber < 9) {
+    		if (($scope.board[idx].moveByPlayer !='X') && ($scope.board[idx].moveByPlayer !='O')){
 
-						if (($scope.turns[0].numMoves % 2) == 0) {
+						if (($scope.turnNumber % 2) == 0) {
 							$scope.board[idx].moveByPlayer = "X";
 							$scope.board.$save($scope.board[idx]);
 						}
-						else if (($scope.turns[0].numMoves % 2) != 0) {
+						else if (($scope.turnNumber % 2) != 0) {
 							$scope.board[idx].moveByPlayer = "O";
 							$scope.board.$save($scope.board[idx]);
 						}
 		        $scope.winConditions();
-		        $scope.turns[0].numMoves++;
-						$scope.turns.$save[0];
+		        $scope.turnNumber++;
 		    }
 		}
     };
@@ -115,73 +112,73 @@ ticTacToeApp.controller("ticTacToeCtrl", function($scope, $firebase) {
 			if (($scope.turnNumber % 2) == 0) {
 	    	if(($scope.board[0].moveByPlayer == "X") && ($scope.board[1].moveByPlayer == "X") && ($scope.board[2].moveByPlayer == "X") ) {
 					$scope.xWins = true;
-					alert("X is a tic tac toe bad ass! X is officially the winner.");
+					alert('"Build your own dreams, or someone else will hire you to build theirs."');
 	    		}
 	    	else if(($scope.board[3].moveByPlayer == "X") && ($scope.board[4].moveByPlayer == "X") && ($scope.board[5].moveByPlayer == "X") ) {
 	    		$scope.xWins = true;
-					alert("X is a tic tac toe bad ass! X is officially the winner.");
+					alert('"It does not matter how slowly you go as long as you do not stop."');
 					}
 	    	else if(($scope.board[6].moveByPlayer == "X") && ($scope.board[7].moveByPlayer == "X") && ($scope.board[8].moveByPlayer == "X") ) {
 	    	  $scope.xWins = true;
-					alert("X is a tic tac toe bad ass! X is officially the winner.");
+					alert('"It’s your place in the world; it’s your life. Go on and do all you can with it, and make it the life you want to live."');
 	    		}
 	    	else if(($scope.board[0].moveByPlayer == "X") && ($scope.board[3].moveByPlayer == "X") && ($scope.board[6].moveByPlayer == "X") ) {
 					$scope.xWins = true;
-					alert("X is a tic tac toe bad ass! X is officially the winner.");
+					alert('"Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do, so throw off the bowlines, sail away from safe harbor, catch the trade winds in your sails. Explore, Dream, Discover."');
 	    	  }
 	    	else if(($scope.board[1].moveByPlayer =="X") && ($scope.board[4].moveByPlayer == "X") && ($scope.board[7].moveByPlayer == "X") ) {
 	    	  $scope.xWins = true;
-					alert("X is a tic tac toe bad ass! X is officially the winner.");
+					alert('"You can never cross the ocean until you have the courage to lose sight of the shore."');
 	    		}
 	    	else if(($scope.board[2].moveByPlayer == "X") && ($scope.board[5].moveByPlayer == "X") && ($scope.board[8].moveByPlayer == "X") ) {
 	    		$scope.xWins = true;
-					alert("X is a tic tac toe bad ass! X is officially the winner.");
+					alert('"Everything you’ve ever wanted is on the other side of fear."');
 	    		}
 	    	else if(($scope.board[0].moveByPlayer == "X") && ($scope.board[4].moveByPlayer == "X") && ($scope.board[8].moveByPlayer == "X") ) {
 					$scope.xWins = true;
-					alert("X is a tic tac toe bad ass! X is officially the winner.");
+					alert('"Certain things catch your eye, but pursue only those that capture the heart."');
 	    	 	}
 	    	else if(($scope.board[2].moveByPlayer == "X") && ($scope.board[4].moveByPlayer == "X") && ($scope.board[6].moveByPlayer == "X") ) {
 					$scope.xWins = true;
-					alert("X is a tic tac toe bad ass! X is officially the winner.");
+					alert('"Challenges are what make life interesting and overcoming them is what makes life meaningful."');
 					}
 			}
 			else if (($scope.turnNumber % 2) != 0) {
 				if(($scope.board[0].moveByPlayer == "O") && ($scope.board[1].moveByPlayer == "O") && ($scope.board[2].moveByPlayer == "O") ) {
 					$scope.oWins = true;
-					alert("O is a tic tac toe bad ass! O is officially the winner.");
+					alert('"Limitations live only in our minds. But if we use our imaginations, our possibilities become limitless."');
 				}
 				else if(($scope.board[3].moveByPlayer == "O") && ($scope.board[4].moveByPlayer == "O") && ($scope.board[5].moveByPlayer == "O") ) {
 					$scope.oWins = true;
-					alert("O is a tic tac toe bad ass! O is officially the winner.");
+					alert('"You may be disappointed if you fail, but you are doomed if you don’t try."');
 					}
 				else if(($scope.board[6].moveByPlayer == "O") && ($scope.board[7].moveByPlayer == "O") && ($scope.board[8].moveByPlayer == "O") ) {
 					$scope.oWins = true;
-					alert("O is a tic tac toe bad ass! O is officially the winner.");
+					alert('"When everything seems to be going against you, remember that the airplane takes off against the wind, not with it."');
 					}
 				else if(($scope.board[0].moveByPlayer == "O") && ($scope.board[3].moveByPlayer == "O") && ($scope.board[6].moveByPlayer == "O") ) {
 					$scope.oWins = true;
-					alert("O is a tic tac toe bad ass! O is officially the winner.");
+					alert('"Either write something worth reading or do something worth writing."');
 					}
 				else if(($scope.board[1].moveByPlayer == "O") && ($scope.board[4].moveByPlayer == "O") && ($scope.board[7].moveByPlayer == "O") ) {
 					$scope.oWins = true;
-					alert("O is a tic tac toe bad ass! O is officially the winner.");
+					alert('"You were born with potential. You were born with goodness and trust. You were born with ideals and dreams. You were born with greatness. You were born with wings. You are not meant for crawling, so don’t. You have wings. Learn to use them and fly."');
 					}
 				else if(($scope.board[2].moveByPlayer == "O") && ($scope.board[5].moveByPlayer == "O") && ($scope.board[8].moveByPlayer == "O") ) {
 					$scope.oWins = true;
-					alert("O is a tic tac toe bad ass! O is officially the winner.");
+					alert('"You are far too smart to be the only thing standing in your way."');
 					}
 				else if(($scope.board[0].moveByPlayer == "O") && ($scope.board[4].moveByPlayer == "O") && ($scope.board[8].moveByPlayer == "O") ) {
 					$scope.oWins = true;
-					alert("O is a tic tac toe bad ass! O is officially the winner.");
+					alert('"Tell the negative committee that meets inside your head to sit down and shut up."');
 					}
 				else if(($scope.board[2].moveByPlayer == "O") && ($scope.board[4].moveByPlayer == "O") && ($scope.board[6].moveByPlayer == "O") ) {
 					$scope.oWins = true;
-					alert("O is a tic tac toe bad ass! O is officially the winner.");
+					alert('"Never let success get to your head, never let failure get to your heart."');
 					}
 			}
 			if (($scope.turnNumber == 8) && ($scope.oWins != true) && ($scope.xWins != true) ) {
-					alert("So sorry Charlie! You haven't outsmarted your opponent...it's a tie.");
+					alert("So sorry Charlie! You haven't outsmarted your opponent, and therefore do not deserve any wisdom. Request a redo, get out of your head, and show us why you merit some advice.");
 				}
 			if (($scope.oWins == true) || ($scope.xWins == true)) {
 					// console.log("now stop game");
